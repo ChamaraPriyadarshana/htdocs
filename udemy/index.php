82598f9d8809a1c468794b0e3c5331410a7a3cb5
Chamara Priyadarshana <chamarapriyadarshana.dev@gmail.com>
2021-05-28 21:07:37 +0530

    Html

--- udemy/index.php
+++ udemy/index.php
@@ -1,3 +1,16 @@
 <?php
-    echo "Hello PHP";
+    //echo "Hello PHP";
+    //phpinfo();
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
